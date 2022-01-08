4b650104a9df4a075f702540a5c385e5bcdc5c88
MunDane <97174547+Extra-Mundane@users.noreply.github.com>
2022-01-08 11:24:03 -0800

    shit game
    Yeah, Cheri won't be posting for awhile so I'm here
    -------------------------------------------------------------
    btw, Change getgenv().PriorityCheck = {
            AutoWin = true
    }
    
    to false if you want to turn off the auto farm

---
+++ Corridor-Of-Youtubers2.lua
@@ -0,0 +1,26 @@
+local winpart = game:GetService("Workspace").Ganha
+local lpname = game:GetService("Players").LocalPlayer.Name
+local lp = game:GetService("Players").LocalPlayer
+local lpsec = game:GetService("Players").LocalPlayer
+getgenv().PriorityCheck = {
+        AutoWin = true
+}
+
+function repeatAfterDied()
+      wait(2)
+      while PriorityCheck.AutoWin == true do
+      wait()
+      lp.Character.HumanoidRootPart.CFrame = winpart.CFrame
+    end
+end
+
+if game:GetService("Players").LocalPlayer.Character.Humanoid.Health == 0 then
+    print("Player is Dead")
+elseif game:GetService("Players").LocalPlayer.Character.Humanoid.Health == 100 then
+    repeatAfterDied()
+end
+
+while PriorityCheck.AutoWin == true do
+wait()
+lp.Character.HumanoidRootPart.CFrame = winpart.CFrame
+end
